9e83759db665473da75414ef51d6ff05f73e18b8
williamesamuelson <william.samuelson@gmail.com>
2023-01-28 08:59:50 +0100

    heatmaps of Kitaev MP and energy gap

--- scripts/firsttest.jl
+++ scripts/firsttest.jl
@@ -5,21 +5,30 @@ using QuantumDots
 using LinearAlgebra
 using Plots
 
-function hamiltonian(particle_ops, Δ, t0, ϵ, ϕ, U, Ez)
+function hamiltonian(particle_ops, Δ, t0, ϵ, λ, U, Vz, Φ)
     d = particle_ops
     N = QuantumDots.nbr_of_fermions(d) ÷ 2
-    t_normal = [t0[j]*cos(ϕ[j]/2) for j in 1:N-1]
-    t_flip = [t0[j]*sin(ϕ[j]/2) for j in 1:N-1]
-
-    ham_dot_sp = ((ϵ[j] + eta(σ)*Ez/2)*d[j,σ]'d[j,σ] for j in 1:N, σ in (:↑, :↓))
+    ham_dot_sp = ((ϵ[j] - eta(σ)*Vz)*d[j,σ]'d[j,σ] for j in 1:N, σ in (:↑, :↓))
     ham_dot_int = (U*d[j,:↑]'d[j,:↑]*d[j,:↓]'d[j,:↓] for j in 1:N)
-    ham_tun_normal = (t_normal[j]*d[j+1, σ]'d[j, σ] for j in 1:N-1, σ in (:↑, :↓))
-    ham_tun_flip = (t_flip[j]*d[j+1, σ]'d[j, ρ] for j in 1:N-1, σ in (:↑, :↓), ρ in (:↑, :↓) if σ != ρ)
-    ham_sc = (Δ[j]*d[j, :↑]'d[j, :↓]' for j in 1:N)
+    ham_tun_normal = (t0[j]*cos(λ[j])*d[j, σ]'d[j+1, σ] for j in 1:N-1, σ in (:↑, :↓))
+    ham_tun_flip = (t0[j]*sin(λ[j])*(d[j, :↑]'d[j+1, :↓] - d[j, :↓]'d[j+1, :↑]) for j in 1:N-1) 
+    ham_sc = (Δ[j]*exp(1im*Φ[j])*d[j, :↑]'d[j, :↓]' for j in 1:N)
     ham = sum(ham_tun_normal) + sum(ham_tun_flip) + sum(ham_sc)
     # add conjugates
     ham += ham'
     ham += sum(ham_dot_sp) + sum(ham_dot_int)
+    return ham
+end
+
+function kitaev(particle_ops, Δ, t0, ϵ)
+    a = particle_ops
+    N = QuantumDots.nbr_of_fermions(a)
+    ham_dot = (ϵ*a[j]'a[j] for j in 1:N)
+    ham_tun = (t0*a[j+1]'a[j] for j in 1:N-1)
+    ham_sc = (Δ*a[j+1]'a[j]' for j in 1:N-1)
+    ham = sum(ham_tun) + sum(ham_sc)
+    ham += ham'
+    ham += sum(ham_dot)
     return ham
 end
 
@@ -36,38 +45,48 @@ function groundindices(basis, vecs, energies)
 end
 
 function majoranapolarization(majoranas, oddstate, evenstate)
-    w, z = map(majorana -> oddstate'*majorana*evenstate, majoranas)
-    return (w^2 - z^2)/(w^2 + z^2)
+    a, b = map(majorana -> oddstate'*majorana*evenstate, majoranas)
+    return a^2 - b^2
 end
 
 function plot_gapandmp()
-    N = 2
+    N = 3
     d = FermionBasis(1:N, (:↑, :↓))
-    t0 = fill(1, N)
-    Δ = 4*t0
+    a = FermionBasis(1:N)
+    t = 1
+    t0 = fill(t, N-1)
+    Δind = fill(20*t, N)
     ϵ = zeros(N)
-    ϕ = fill(π/2, N)
-    U = 20*t0[1]
-    Ez = 4*t0[1]
+    λ = fill(π/2, N-1)
+    U = 0
+    Vz = 20*t
+    Φ = zeros(N)
 
     points = 100
-    eps_vec = range(-2Δ[1], 2Δ[1], points)
-    gaps = zeros(Float64, points)
-    # mps = zeros(Float64, points)
-    # maj_plus = d[1] + d[1]'
-    # maj_minus = d[1] - d[1]'
-    # majoranas = (maj_plus, maj_minus)
+    eps_vec = range(-2t, 2t, points)
+    delta_vec = range(0, 2t, points)
+    gaps = zeros(Float64, points, points)
+    mps = zeros(Float64, points, points)
+    maj_plus = a[1] + a[1]'
+    maj_minus = a[1] - a[1]'
+    majoranas = (maj_plus, maj_minus)
     for i = 1:points
-        fill!(ϵ, eps_vec[i])
-        ham = hamiltonian(d, Δ, t0, ϵ, ϕ, U, Ez)
-        energies, vecs = eigen!(Matrix(ham))
-        even, odd = groundindices(d, eachcol(vecs), energies)
-        gaps[i] = energies[even] - energies[odd]
-        # mps[i] = majoranapolarization(majoranas, vecs[:,odd], vecs[:,even])
+        for j = 1:points
+            fill!(ϵ, eps_vec[i])
+            # ham = hamiltonian(d, Δind, t0, ϵ, λ, U, Vz, Φ)
+            ham_kitaev = kitaev(a, delta_vec[j], t, eps_vec[i]) 
+            energies, vecs = eigen!(Matrix{ComplexF64}(ham_kitaev))
+            even, odd = groundindices(a, eachcol(vecs), energies)
+            gaps[i, j] = energies[even] - energies[odd]
+            mps[i, j] = majoranapolarization(majoranas, vecs[:,odd], vecs[:,even])
+        end
     end
     # display(plot(eps_vec, [gaps, mps], label=["Gap" "MP"]))
-    display(plot(eps_vec, gaps))
-    xlabel!("Dot energies")
+    gapsplot = heatmap(delta_vec, eps_vec, gaps, color = :balance)
+    mpplot = heatmap(delta_vec, eps_vec, abs.(mps), color = :plasma)
+    display(plot(gapsplot, mpplot, layout = 2))
+    xlabel!("energy")
+    ylabel!("SC gap")
 end
 end
 
