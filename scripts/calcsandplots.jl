bea29a60267930321f9d505bf0d4a489fbfbe102
williamesamuelson <william.samuelson@gmail.com>
2023-02-18 22:38:02 +0100

    implementing 2d scan

--- scripts/calcsandplots.jl
+++ scripts/calcsandplots.jl
@@ -6,6 +6,7 @@ using LinearAlgebra
 using Plots
 using MajoranaFunctions
 using LaTeXStrings
+using Roots
 
 function scan_gapmp_1d(scan_params::Dict{String, T}, fix_params, ham_fun, points, sites) where T<:Vector
     if ham_fun == kitaev
@@ -30,10 +31,41 @@ function scan_gapmp_1d(scan_params::Dict{String, T}, fix_params, ham_fun, points
     return gaps, mps
 end
 
+function scan_gapmp_2d(xscan_params, yscan_params, fix_params, ham_fun, points, sites)
+    if ham_fun == kitaev
+        d = FermionBasis(1:sites)
+    else
+        d = FermionBasis((1:sites), (:↑, :↓))
+    end
+    gaps = zeros(Float64, points, points)
+    mps = zeros(Float64, points, points)
+    xformattedscan = format_scan(xscan_params, sites)
+    yformattedscan = format_scan(yscan_params, sites)
+    params = merge(xformattedscan, yformattedscan, fix_params)
+    for i = 1:points
+        for (p, val) in xscan_params
+            fill!(params[p], val[i])
+        end
+        for j = 1:points
+            for (p, val) in yscan_params
+                fill!(params[p], val[j])
+            end
+            H = ham_fun(d, params)
+            energies, vecs = eigen!(Matrix{ComplexF64}(H))
+            even, odd = groundindices(d, eachcol(vecs), energies)
+            gaps[i, j] = energies[even] - energies[odd]
+            mps[i, j] = majoranapolarization(d, vecs[:,odd], vecs[:,even])
+        end
+    end
+    return gaps, mps
+end
+
 function format_scan(scan_params, N)
     lengths = MajoranaFunctions.lengthofparams(N)
     return Dict(p => zeros(lengths[p]) for p in keys(scan_params))
 end
+
+getfunc(Δind, V, U) = μ -> V - √(μ^2+Δind^2) + U*μ/√(μ^2 + Δind^2)
 
 function localvskitaev()
     sites = 4
@@ -50,9 +82,9 @@ function localvskitaev()
     λ = atan.(Δ*tan(2α)/t0)
     w = t0./(cos.(λ)*sin(2α))
     Φ = fill(0, sites)
-    Uscal = 1
+    Uscal = 1.5
     U = fill(Uscal*t0, sites)
-    Vzscalings = [1 5 1e7]
+    Vzscalings = [1e5]
     innerplots = []
 	plot_font = "Computer Modern"
 	default(fontfamily=plot_font, linewidth=2, framestyle=:box,
@@ -61,8 +93,9 @@ function localvskitaev()
     scalefontsizes(0.8)
     for Vzscal in Vzscalings
         Vz = fill(Vzscal*t0, sites)
-        μ = Vz*sin(2α) + U
         Δind = Vz*cos(2α)
+        func = getfunc(Δind[1], Vz[1], U[1])
+        μ = fill(find_zero(func, Vz[1]+U[1]), sites)  
         scan_params = Dict("λ"=>λ, "w"=>w)
         fix_params = Dict("μ"=>μ, "Δind"=>Δind, "Φ"=>Φ, "U"=>U, "Vz"=>Vz) 
         gap, mp = scan_gapmp_1d(scan_params, fix_params, localpairingham, points, sites)
@@ -71,33 +104,45 @@ function localvskitaev()
         push!(innerplots, plot(gapplot, mpplot,
                                xaxis=L"Δ/t", lw=2, title=L"V_z=%$Vzscal t", dpi=300))
     end
-    p = plot((pl for pl in innerplots)..., layout=(4,1), plot_title=L"U=%$Uscal t")
+    p = plot((pl for pl in innerplots)..., layout=(1,1), plot_title=L"U=%$Uscal t")
     display(plot(p))
     # params = @strdict sites α Uscal
     # save = "localvskitaev"*savename(params)
     # png(plotsdir("localvskitaev", save))
 end
 
-function main()
-    sites = 2
-    w0 = 0.25
+function twodimscan()
+    sites = 3
+    points = 50
+    w0 = 1
+    t0 = 1
+    t = fill(t0, sites-1)
+    Δ = collect(range(-2t0, 2t0, points))
     w = fill(w0, sites-1)
-    Vz0 = 1e5*w0
-    Vz = fill(Vz0, sites)
     α = 0.2*π
-    λ = fill(-1.2566, sites-1)
-    μ = fill(Vz0*sin(2*α), sites)
-    Δind = fill(Vz0*cos(2*α), sites)
     Φ = fill(0, sites)
-    U = fill(0, sites)
-    # params = Dict("μ"=>μ, "Δind"=>Δind, "w"=>w, "Φ"=>Φ, "U"=>U, "Vz"=>Vz, "λ"=>λ)
-    params = Dict("ϵ"=>μ, "Δ"=>Δind, "t"=>w)
-    # d = FermionBasis((1:sites), (:↑, :↓))
-    d = FermionBasis(1:sites)
-    ham = kitaev(d, params)
-    energies, vecs = eigen!(Matrix{ComplexF64}(ham))
-    even, odd = groundindices(d, eachcol(vecs), energies)
-    majoranapolarization(d, vecs[:, odd], vecs[:, even])
+    λ = atan.(Δ*tan(2α)/t0)
+    Uscal = 0
+    U = fill(Uscal*w0, sites)
+    Vzscal = 1e4
+    Vz = fill(Vzscal*t0, sites)
+    Δind = Vz*cos(2α)
+    dμ = 3*w0
+    func = getfunc(Δind[1], Vz[1], U[1])
+    μguess = find_zero(func, Vz[1]+U[1])
+    μ = collect(range(μguess-dμ, μguess+dμ, points))
+    xscan_params = Dict("λ"=>λ)
+    yscan_params = Dict("μ"=>μ)
+    fix_params = Dict("w"=>w, "Δind"=>Δind, "Φ"=>Φ, "U"=>U, "Vz"=>Vz) 
+    gap, mp = scan_gapmp_2d(xscan_params, yscan_params, fix_params, localpairingham, points, sites)
+	plot_font = "Computer Modern"
+	default(fontfamily=plot_font, linewidth=2, framestyle=:box,
+            label=nothing, grid=false)
+    scalefontsizes()
+    scalefontsizes(0.8)
+    gapplot = heatmap(λ, μ, gap, c=:seismic, clims=(-abs(maximum(gap)), abs(maximum(gap))))
+    mpplot = heatmap(λ, μ, abs.(mp), c=:seismic)
+    display(plot(gapplot, mpplot, layout=(1,2)))
 end
 end
 
