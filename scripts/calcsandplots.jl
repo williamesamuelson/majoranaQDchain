b9ee4ad5a34dec42011e457dc85d63eaea2ba79a
williamesamuelson <william.samuelson@gmail.com>
2023-02-22 16:00:43 +0100

    2d scans

--- scripts/calcsandplots.jl
+++ scripts/calcsandplots.jl
@@ -43,11 +43,11 @@ function scan_gapmp_2d(xscan_params, yscan_params, fix_params, ham_fun, points, 
     yformattedscan = format_scan(yscan_params, sites)
     params = merge(xformattedscan, yformattedscan, fix_params)
     for i = 1:points
-        for (p, val) in xscan_params
+        for (p, val) in yscan_params
             fill!(params[p], val[i])
         end
         for j = 1:points
-            for (p, val) in yscan_params
+            for (p, val) in xscan_params
                 fill!(params[p], val[j])
             end
             H = ham_fun(d, params)
@@ -65,10 +65,10 @@ function format_scan(scan_params, N)
     return Dict(p => zeros(lengths[p]) for p in keys(scan_params))
 end
 
-getfunc(Δind, V, U) = μ -> V - √(μ^2+Δind^2) + U*μ/√(μ^2 + Δind^2)
+getfunc(Δind, V, U) = (μ -> -V + √(μ^2+Δind^2) + U*μ/√(μ^2 + Δind^2))
 
 function localvskitaev()
-    sites = 4
+    sites = 2
     points = 100
     t0 = 1
     t = fill(t0, sites-1)
@@ -82,7 +82,7 @@ function localvskitaev()
     λ = atan.(Δ*tan(2α)/t0)
     w = t0./(cos.(λ)*sin(2α))
     Φ = fill(0, sites)
-    Uscal = 1.5
+    Uscal = 10
     U = fill(Uscal*t0, sites)
     Vzscalings = [1e5]
     innerplots = []
@@ -112,37 +112,37 @@ function localvskitaev()
 end
 
 function twodimscan()
-    sites = 3
+    sites = 2
     points = 50
     w0 = 1
     t0 = 1
     t = fill(t0, sites-1)
     Δ = collect(range(-2t0, 2t0, points))
-    w = fill(w0, sites-1)
     α = 0.2*π
     Φ = fill(0, sites)
     λ = atan.(Δ*tan(2α)/t0)
-    Uscal = 0
-    U = fill(Uscal*w0, sites)
-    Vzscal = 1e4
+    w = t0./(cos.(λ)*sin(2α))
+    Uscal = 5
+    U = fill(Uscal*t0, sites)
+    Vzscal = 1e5
     Vz = fill(Vzscal*t0, sites)
     Δind = Vz*cos(2α)
-    dμ = 3*w0
+    dμ = 6*w0
     func = getfunc(Δind[1], Vz[1], U[1])
     μguess = find_zero(func, Vz[1]+U[1])
     μ = collect(range(μguess-dμ, μguess+dμ, points))
-    xscan_params = Dict("λ"=>λ)
+    xscan_params = Dict("λ"=>λ, "w"=>w)
     yscan_params = Dict("μ"=>μ)
-    fix_params = Dict("w"=>w, "Δind"=>Δind, "Φ"=>Φ, "U"=>U, "Vz"=>Vz) 
+    fix_params = Dict("Δind"=>Δind, "Φ"=>Φ, "U"=>U, "Vz"=>Vz) 
     gap, mp = scan_gapmp_2d(xscan_params, yscan_params, fix_params, localpairingham, points, sites)
 	plot_font = "Computer Modern"
 	default(fontfamily=plot_font, linewidth=2, framestyle=:box,
             label=nothing, grid=false)
     scalefontsizes()
     scalefontsizes(0.8)
-    gapplot = heatmap(λ, μ, gap, c=:seismic, clims=(-abs(maximum(gap)), abs(maximum(gap))))
-    mpplot = heatmap(λ, μ, abs.(mp), c=:seismic)
-    display(plot(gapplot, mpplot, layout=(1,2)))
+    p = heatmap(Δ, μ, abs.(mp), c=:BuPu) 
+    contour!(p, Δ, μ, gap, levels=[0.0], c=:green, colorbar_entry=false)
+    display(plot(p)) 
 end
 end
 
