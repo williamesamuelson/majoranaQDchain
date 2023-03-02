51cb3f9129343895ab33a4c0f5a9630d208dca40
williamesamuelson <william.samuelson@gmail.com>
2023-03-02 16:03:46 +0100

    converting to named tuples, new delta rho and optimization

--- scripts/calcsandplots.jl
+++ scripts/calcsandplots.jl
@@ -50,41 +50,39 @@ function kitaevtoakhmerovparams(t, Δ, α)
     return w, λ
 end
 
-function particleenergyfunc(Δind, V, U)
-    function f(μ)
-        β = √(μ^2+Δind^2)
-        return -V + β - U*(β-μ)/(2*β)
-    end
-    return f
+function zeroenergy_condition(μ, Δind, Vz, U)
+    β = √(μ^2+Δind^2)
+    return -V + β - U*(β-μ)/(2*β)
 end
 
 function μguess(Δind, Vz, U)
-    func = particleenergyfunc(Δind, Vz, U)
-    μguess = √(Vz^2-Δind^2)
-    μval = find_zero(func, μguess)
+    f(μ) = zeroenergy_condition(μ, Δind, Vz, U)
+    μinit = √(Vz^2-Δind^2)
+    return find_zero(f, μinit)
 end
 
-function create_sweetspot_optfunc(particle_ops, params, t, α)
+function create_sweetspot_optfunc(particle_ops, params, scansyms, weight, ϵ)
     function sweetspotfunc(x)
-        Δ = x[1]
-        μ = x[2]
-        w, λ = kitaevtoakhmerovparams(t, Δ, α)
-        params["μ"], params["w"], params["λ"] = μ, w, λ
+        for i in 1:length(scansyms)
+            params[scansyms[i]] = x[i]
+        end
         gap, mp, dρsq = measures(particle_ops, localpairingham, params)
-        return dρsq + gap^2
+        return dρsq + weight*(abs(gap) - ϵ)^2
     end
     return sweetspotfunc
 end
 
-function optimizesweetspot(particle_ops, params, t, α, range, guess, maxtime)
-    opt_func = create_sweetspot_optfunc(particle_ops, params, t, α)
-    res = bboptimize(opt_func, guess, SearchRange=range, TraceMode=:silent,
+# should make a function for the middle rows
+function optimizesweetspot(particle_ops, params::Dict{Symbol, T}, scansyms::NTuple{M, Symbol}, guess, range, maxtime) where {T,M}
+    opt_func = create_sweetspot_optfunc(particle_ops, params, scansyms, 1e6, 1e-8)
+    res = bboptimize(opt_func, guess, SearchRange=range, TraceMode=:compact,
                      MaxTime=maxtime)
-    Δ, μ = best_candidate(res)
-    w, λ = kitaevtoakhmerovparams(t, Δ, α)
-    params["μ"], params["w"], params["λ"] = μ, w, λ
+    point = best_candidate(res)
+    for i in 1:length(scansyms)
+        params[scansyms[i]] = point[i]
+    end
     gap, mp, dρsq = measures(particle_ops, localpairingham, params)
-    return [Δ, μ], gap, dρsq
+    return point, gap, dρsq
 end
 
 function initializeplot()
@@ -97,39 +95,38 @@ end
 function twodimscan()
     sites = 2
     d = FermionBasis((1:sites), (:↑, :↓))
-    points = 10
-    # Kitaev params
-    t = 1
-    Δ = collect(range(-2t, 2t, points))
-    # Akhmerov params
-    α = 0.2*π
-    w, λ = kitaevtoakhmerovparams(t, Δ, α)
-    Φ = 0
-    U = 1
-    Vz = 3
-    Δind = Vz*cos(2α)
-    dμ = 2*t
-    μval = μguess(Δind, Vz, U)
-    μ = collect(range(μval-dμ, μval+dμ, points))
-    xparams = Dict("λ"=>λ, "w"=>w)
-    yparams = Dict("μ"=>μ)
-    fix_params = Dict("Δind"=>Δind, "Φ"=>Φ, "U"=>U, "Vz"=>Vz) 
+    points = 100
+    w = 1.0
+    λ = π/4
+    Φ = 0w
+    U = 10w
+    Vz = 1e3w
+    Δindval = Vz*cos(λ)
+    μval = Vz*sin(λ)
+    dμ = 10w
+    dΔind = 10w
+    # μ = collect(range(0.1, Vz, points))
+    # Δind = collect(range(0.1, Vz, points))
+    μ = collect(range(μval - dμ, μval + dμ, points))
+    Δind = collect(range(Δindval - dΔind, Δindval + dΔind, points))
+    xparams = Dict(:Δind=>Δind)
+    yparams = Dict(:μ=>μ)
+    fix_params = Dict(:w=>w, :λ=>λ, :Φ=>Φ, :U=>U, :Vz=>Vz) 
     @time gap, mp, dρsq = scan2d(xparams, yparams, fix_params, d, localpairingham, points, sites)
-    opt_params = Dict("μ"=>0, "w"=>0, "λ"=>0)
-    ssguess = [t, μval]
-    @time sweetspot, gapss, dρsqss = optimizesweetspot(d, merge(fix_params, opt_params), t, α,
-                                                       [(Δ[end ÷ 2], Δ[end]), (μ[1], μ[end])], ssguess, 100)
+    opt_params = merge(fix_params, Dict(:μ=>0, :Δind=>0))
+    sweetspot, gapss, dρsqss = optimizesweetspot(d, opt_params, (:Δind, :μ), [Δindval, μval], [(Δind[1], Δind[end]), 
+                                                                                           (μ[1], μ[end])], 10)
     initializeplot()
-    p = heatmap(Δ, μ.-μval, dρsq, c=:acton)
+    p = heatmap(Δind, μ, dρsq, c=:acton)
     contourlvl = 0.05
     lvls = [[-contourlvl], [0.0], [contourlvl]]
     clrs = [:green4, :lightgreen, :green4]
     for i in 1:length(lvls)
-        contour!(p, Δ, μ.-μval, gap, levels=lvls[i], c=clrs[i], colorbar_entry=false)
+        contour!(p, Δind, μ, gap, levels=lvls[i], c=clrs[i], colorbar_entry=false)
     end
-    scatter!(p, [sweetspot[1]], [sweetspot[2] - μval], c=:cyan, legend=false)
-    # scatter!(p, [t], [0], c=:red)
-    display(plot(p, xlabel=L"\Delta_{Kitaev}(w, \lambda)/t_{Kitaev}", ylabel=L"(\mu-\mu_{ss})/t_{Kitaev}",
+    scatter!(p, [sweetspot[1]], [sweetspot[2]], c=:cyan, legend=false)
+    scatter!(p, [Δindval], [μval], c=:red, legend=false)
+    display(plot(p, xlabel=L"\Delta_{ind}/w", ylabel=L"\mu/w",
                  colorbar_title=L"||\delta \rho_R||^2", title=L"N=%$sites, U=%$U, V_z=%$Vz"))
     # params = @strdict sites U Vz
     # save = "2dscan"*savename(params)
@@ -144,9 +141,9 @@ function sweetspotzeeman(sites, params, Vz::Vector, t, α)
     dρs = zeros(points)
     for j in 1:points
         Δind = Vz[j]*cos(2α)
-        ssguess = [t, μguess(Δind, Vz[j], params["U"])]
-        params["Vz"], params["Δind"] = Vz[j], Δind
-        diffs = [3t, 2t]
+        ssguess = [t, μguess(Δind, Vz[j], params[:U])]
+        params[:Vz], params[:Δind] = Vz[j], Δind
+        diffs = [3t, 3t]
         range = [(ssguess[i] - diffs[i], ssguess[i] + diffs[i]) for i in 1:2]
         _, gaps[j], dρs[j] = optimizesweetspot(d, params, t, α, range, ssguess, 20)
     end
@@ -162,11 +159,10 @@ function calc()
     Φ = 0
     U = 1
     Vz = [3, 10, 1e2, 1e3, 1e4]
-    Δind = Vz*cos(2α)
-    params = Dict("μ"=>0, "w"=>0, "λ"=>0, "Δind"=>Δind, "Φ"=>Φ, "U"=>U, "Vz"=>0) 
+    params = Dict{Symbol, Float64}(:μ=>0, :w=>0, :λ=>0, :Δind=>0, :Φ=>Φ, :U=>U, :Vz=>0) 
     gaps, dρs = sweetspotzeeman(sites, params, Vz, t, α)
     initializeplot()
-    p = plot(Vz, [gaps.^2 dρs], label=[L"\delta E" L"\delta \rho"], xscale=:log10, yscale=:log10, 
+    p = plot(Vz, [abs.(gaps) dρs], label=[L"\delta E" L"\delta \rho"], xscale=:log10, yscale=:log10, 
              markershape=:auto, xlabel=L"V_z/t_{Kitaev}", title=L"N=%$sites, U=%$U")
     # params = @strdict sites U
     # save = "ssvarzeeman"*savename(params)
--- src/MajoranaFunctions/src/functions.jl
+++ src/MajoranaFunctions/src/functions.jl
@@ -1,35 +1,11 @@
-function lengthofparams(N)
-    return Dict("μ"=>N, "Δind"=>N, "w"=>N-1, "λ"=>N-1, "Φ"=>N, "U"=>N, "Vz"=>N,
-                "ϵ"=>N, "Δ"=>N, "t"=>N-1)
+function lengthofparams(sites)
+    return (μ=sites, Δind=sites, w=sites-1, λ=sites-1, Φ=sites, U=sites, Vz=sites,
+                ϵ=sites, Δ=sites, t=sites-1)
 end
 
-function localpairingham(particle_ops, params::Dict{String, Vector})
-    d = particle_ops
-    N = QuantumDots.nbr_of_fermions(d) ÷ 2
-    p = params
-    μ = p["μ"]
-    Δind = p["Δind"]
-    w = p["w"]
-    λ = p["λ"]
-    Φ = p["Φ"]
-    U = p["U"]
-    Vz = p["Vz"]
-    ham_dot_sp = ((μ[j] - η(σ)*Vz[j])*d[j,σ]'d[j,σ] for j in 1:N, σ in (:↑, :↓))
-    ham_dot_int = (U[j]*d[j,:↑]'d[j,:↑]*d[j,:↓]'d[j,:↓] for j in 1:N)
-    ham_tun_normal = (w[j]*cos(λ[j])*d[j, σ]'d[j+1, σ] for j in 1:N-1, σ in (:↑, :↓))
-    ham_tun_flip = (w[j]*sin(λ[j])*(d[j, :↑]'d[j+1, :↓] - d[j, :↓]'d[j+1, :↑]) for j in 1:N-1) 
-    ham_sc = (Δind[j]*exp(1im*Φ[j])*d[j, :↑]'d[j, :↓]' for j in 1:N)
-    ham = sum(ham_tun_normal) + sum(ham_tun_flip) + sum(ham_sc)
-    # add conjugates
-    ham += ham'
-    ham += sum(ham_dot_sp) + sum(ham_dot_int)
-    return ham
-end
-
-function localpairingham(particle_ops, params)
-    N = QuantumDots.nbr_of_fermions(particle_ops) ÷ 2
-    lop = lengthofparams(N)
-    newparams = Dict{String, Vector}(p=>zeros(lop[p]) for p in keys(params))
+function convert_to_namedtuple(params, sites)
+    lop = lengthofparams(sites)
+    newparams = Dict(p=>zeros(Float64, lop[p]) for p in keys(params))
     for (p, val) in params
         if val isa Number
             fill!(newparams[p], val)
@@ -37,30 +13,65 @@ function localpairingham(particle_ops, params)
             newparams[p] = val 
         end
     end
+    return (; newparams...)
+end
+
+function localpairingham(particle_ops, params::NamedTuple{S, NTuple{7, Vector{Float64}}}) where S
+    d = particle_ops
+    sites = QuantumDots.nbr_of_fermions(d) ÷ 2
+    p = params
+    μ = p.μ
+    Δind = p.Δind
+    w = p.w
+    λ = p.λ
+    Φ = p.Φ
+    U = p.U
+    Vz = p.Vz
+    ham_dot_sp = ((μ[j] - η(σ)*Vz[j])*d[j,σ]'d[j,σ] for j in 1:sites, σ in (:↑, :↓))
+    ham_dot_int = (U[j]*d[j,:↑]'d[j,:↑]*d[j,:↓]'d[j,:↓] for j in 1:sites)
+    ham_tun_normal = (w[j]*cos(λ[j])*d[j, σ]'d[j+1, σ] for j in 1:sites-1, σ in (:↑, :↓))
+    ham_tun_flip = (w[j]*sin(λ[j])*(d[j, :↑]'d[j+1, :↓] - d[j, :↓]'d[j+1, :↑]) for j in 1:sites-1) 
+    ham_sc = (Δind[j]*exp(1im*Φ[j])*d[j, :↑]'d[j, :↓]' for j in 1:sites)
+    ham = sum(ham_tun_normal) + sum(ham_tun_flip) + sum(ham_sc)
+    # add conjugates
+    ham += ham'
+    ham += sum(ham_dot_sp) + sum(ham_dot_int)
+    return ham
+end
+
+function localpairingham(particle_ops, params::Dict{Symbol, T}) where T
+    sites = QuantumDots.nbr_of_fermions(particle_ops) ÷ 2
+    newparams = convert_to_namedtuple(params, sites)
     return localpairingham(particle_ops, newparams)
 end
 
 η(spin) = spin == :↑ ? -1 : 1
 
-function kitaev(particle_ops, params::Dict{String, Vector})
+function kitaev(particle_ops, params::NamedTuple{S, NTuple{3, Vector{Float64}}}) where S
     d = particle_ops
-    N = QuantumDots.nbr_of_fermions(d)
+    sites = QuantumDots.nbr_of_fermions(d)
     p = params
-    ϵ = p["ϵ"]
-    Δ = p["Δ"]
-    t = p["t"]
-    ham_dot = (ϵ[j]*d[j]'d[j] for j in 1:N)
-    ham_tun = (t[j]*d[j+1]'d[j] for j in 1:N-1)
-    ham_sc = (Δ[j]*d[j+1]'d[j]' for j in 1:N-1)
+    ϵ = p.ϵ
+    Δ = p.Δ
+    t = p.t
+    ham_dot = (ϵ[j]*d[j]'d[j] for j in 1:sites)
+    ham_tun = (t[j]*d[j+1]'d[j] for j in 1:sites-1)
+    ham_sc = (Δ[j]*d[j+1]'d[j]' for j in 1:sites-1)
     ham = sum(ham_tun) + sum(ham_sc)
     ham += ham'
     ham += sum(ham_dot)
     return ham
 end
 
+function kitaev(particle_ops, params::Dict{Symbol, T}) where T
+    sites = QuantumDots.nbr_of_fermions(particle_ops)
+    newparams = convert_to_namedtuple(params, sites)
+    return localpairingham(particle_ops, newparams)
+end
+
 function measures(particle_ops, ham_fun, params)
         H = ham_fun(particle_ops, params)
-        energies, vecs = eigen!(Hermitian(Matrix(H)))
+        energies, vecs = eigen!(Matrix(H))
         even, odd = groundindices(particle_ops, eachcol(vecs), energies)
         top_gap = energies[3] - energies[1]
         gap = (energies[even] - energies[odd])/top_gap #normalize to gap
@@ -109,33 +120,28 @@ function majoranapolarization(particle_ops::FermionBasis{M, S, T, Sym}, oddstate
 end
 
 function majoranapolarization(particle_ops, ham)
-    energies, vecs = eigen!(Hermitian(Matrix(ham)))
+    energies, vecs = eigen!(Matrix(ham))
     even, odd = groundindices(particle_ops, eachcol(vecs), energies)
     return majoranapolarization(particle_ops, vecs[:, odd], vecs[:, even])
 end
 
 function robustness(particle_ops, oddstate, evenstate, labels)
-    labels = ntuple(i->labels[i], length(labels))
-    ρe, ρo = map(ψ -> QuantumDots.reduced_density_matrix(ψ, labels, particle_ops),
-                 (evenstate, oddstate))
-    return norm(ρe - ρo)^2
+    dρ = 0
+    sites = length(labels)
+    for j in 1:sites
+        remove_indices = [i for i in 1:sites if i != j]
+        remove_labels = ntuple(i->labels[remove_indices[i]], sites-1)
+        ρe, ρo = map(ψ -> QuantumDots.reduced_density_matrix(ψ, remove_labels, particle_ops),
+                    (evenstate, oddstate))
+        dρ += norm(ρe - ρo)^2
+    end
+    return dρ/sites
 end
 
 function robustness(particle_ops::FermionBasis{M, S, T, Sym}, oddstate, evenstate) where {M, S<:Tuple, T, Sym}
     sites = QuantumDots.nbr_of_fermions(particle_ops) ÷ 2
-    n = sites÷2 + sites % 2 + 1
-    labels = collect((i,σ) for i in n:sites, σ in (:↑, :↓)) # correct??
+    labels = collect((i,σ) for i in 1:sites, σ in (:↑, :↓)) # correct??
     return robustness(particle_ops, oddstate, evenstate, labels)
 end
 
-function robustness(particle_ops::FermionBasis{M, S, T, Sym}, oddstate, evenstate) where {M, S<:Number, T, Sym}
-    sites = QuantumDots.nbr_of_fermions(particle_ops)
-    n = sites÷2 + sites % 2  + 1 # sum over half of the sites plus middle if odd
-    labels = collect(i in n:sites) # correct??
-    return robustness(particle_ops, oddstate, evenstate, labels)
-end
-
-function sweetspotguess(gap, dρsq, Δ, μ)
-    ind = argmin(gap.^2 .+ dρsq)
-    return [Δ[ind[2]], μ[ind[1]]]
-end
+robustness(particle_ops::FermionBasis{M, S, T, Sym}, oddstate, evenstate) where {M, S<:Number, T, Sym} = robustness(keys(particle_ops.dict), oddstate, evenstate, labels)
